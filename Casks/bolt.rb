76c1529c12ade8d0ea918988739e4b2a8a471b34
sparcle-eng <249549243+sparcle-eng@users.noreply.github.com>
2026-07-28 16:18:43 -0700

    bolt 0.1.141

--- Casks/bolt.rb
+++ Casks/bolt.rb
@@ -1,7 +1,7 @@
 cask "bolt" do
   version "0.1.141"
-  sha256 arm:   "3d37fa88bba94b011bcbe24cdc572deca98239ae9b2724af9c3dd29021eaf5dc",
-         intel: "0547dc284dd84484db3055d48a213827c3b62ed5a96820cb9d96fc76bab91e88"
+  sha256 arm:   "bc7cd0e1c95e4bc69fa7b021d1d5786e5774d6ab0fd565e6c0683ce2c703121c",
+         intel: "f4bb01e15f7b5e24f252981574b87a70bb30829cc6167f9ad0991e883ad85a15"
 
   url "https://github.com/SparcleHQ/sparcle.app/releases/download/v0.1.141/Bolt-Enterprise-#{version}-#{Hardware::CPU.arm? ? "aarch64" : "x86_64"}-apple-darwin.dmg"
   name "Bolt"
